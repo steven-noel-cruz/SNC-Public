439118b9f50ec9e798ed8dfdb05d76a96d94b45b
Steven Cruz <Stevennoelcallcruz@gmail.com>
2025-02-07 00:54:19 +0000

    Update __template_remediation-STIG-ID-WN10-AU-000500.ps1

--- __template_remediation-STIG-ID-WN10-AU-000500.ps1
+++ __template_remediation-STIG-ID-WN10-AU-000500.ps1
@@ -14,10 +14,10 @@
     STIG-ID         : WN10-AU-000500
 
 .TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
-    Systems Tested  : 
-    PowerShell Ver. : 
+    Date(s) Tested  : 2025-02-07
+    Tested By       : Steven Cruz
+    Systems Tested  : Windows (Windows 10 Pro)
+    PowerShell Ver. : Powershell 5.1
 
 .USAGE
     Put any usage instructions here.
@@ -25,4 +25,19 @@
     PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
 #>
 
-# YOUR CODE GOES HERE
+# Define the registry path and value
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+$valueName = "MaxSize"
+$valueData = 32768
+
+# Check if the registry path exists, if not, create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog" -Name "Application" -Force
+}
+
+# Set the registry value (REG_DWORD)
+New-ItemProperty -Path $registryPath -Name $valueName -PropertyType DWORD -Value $valueData -Force
+
+# Output the result
+Write-Host "Registry key and value have been set successfully."
+
